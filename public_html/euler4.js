a766d0c755890dd84d52c40843580a49b5196725
sean thrasher <s.thrasher64@gmail.com>
2017-05-01 01:53:41 +0000

    New Euler scripts added (5 and 6). Personal header added to index.html.

---
+++ public_html/euler4.js
@@ -0,0 +1,16 @@
+function findLargestPal(num) {
+
+  var num1 = x;
+  var num2 = y;
+  var possibleNum = [];
+
+  for (i=1; i<1000; i++);
+    
+    if (
+
+
+function showLargestPal() {
+
+  document.getElementById("output").innerHTML = findLargestPal(num);
+
+}
