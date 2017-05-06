7586f4876efdce968672e9bf8bc5c1bae368de42
sean thrasher <s.thrasher64@gmail.com>
2017-05-06 22:44:10 +0000

    renamed "resultNum" to "result" in euler1.js.

--- public_html/euler1.js
+++ public_html/euler1.js
@@ -1,17 +1,17 @@
 function getResult() {
 
-  var resultNum = 0;
+  var result = 0;
 
   for (i = 3; i <= 999; i++) {
 
     if ((i % 3 == 0) || (i % 5 == 0)) {
   
-      resultNum = resultNum + i;
+      result = result + i;
       
     }
 
   }
 
-  document.getElementById("output").innerHTML = resultNum;
+  document.getElementById("output").innerHTML = result;
 
 }
